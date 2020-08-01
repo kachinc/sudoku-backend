85a5047c430e7f88509d50455c51f23165b4f06e
Johnson Chong <chongkachin@hotmail.com>
2020-08-02 01:43:45 +0800

    init

---
+++ src/test/java/com/github/kachinc/sudokubackend/SudokuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kachinc.sudokubackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokuBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
