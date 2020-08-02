339bf63f40f6efcc88c9138df7800d785a99df68
kachinc <kcchongab@connect.ust.hk>
2020-08-03 01:04:45 +0800

    make board beautiful

--- src/main/resources/static/vue/home.vue.js
+++ src/main/resources/static/vue/home.vue.js
@@ -16,6 +16,14 @@ const NewGame = {
 	      diff: 0
 	    }
 	},
+	computed: {
+		diffMsg(){
+			if(this.diff >= (1-(17/81))){
+				return 'Less than 17 clues'
+			}
+			return ''
+		}
+	},
 	methods: {
 		createNewGame(){
 			router.push({ name: 'ingame', params: { diff: this.diff } });
@@ -24,9 +32,10 @@ const NewGame = {
 	template: `
 	<div>
 		<h3>Select difficulty</h3>
-		<div>
+		<div class="m-2">
 			<b-form-input id="range-2" v-model="diff" type="range" min="0" max="1" step="0.01"></b-form-input>
 			<span>Difficulty (0 to 1): {{ diff }}</span>
+			<div class="alertMsg">{{diffMsg}}</div>
 		</div>
 		<div>
 			<b-btn variant="success" @click="createNewGame()">Create New Game</b-btn>
@@ -41,7 +50,8 @@ const InGame = {
 		return {
 			diff: this.$route.params.diff,
 			board: [],
-			boardDisabledFlag:[]
+			boardDisabledFlag:[],
+			pickCellValueCellIndex:0
 		}
 	},
 	mounted (){
@@ -87,6 +97,15 @@ const InGame = {
 		checkCellState(i,j){
 			let regex = RegExp('^[1-9]$');
 			return regex.test(this.board[j + 9*i]) ? null : false;
+		},
+		pickCellValue(i,j){
+			this.pickCellValueCellIndex = j + 9*i;
+			this.$bvModal.show('pick-cell-modal');
+		},
+		numPadPressed(num){
+			this.board[this.pickCellValueCellIndex] = num;
+			this.$bvModal.hide('pick-cell-modal');
+			this.$forceUpdate();
 		}
 	},
 	template: `
@@ -95,11 +114,13 @@ const InGame = {
 		<h3>Difficulty of this game: {{diff}}</h3>
 		<b-alert show variant="danger">The problem generated is not correct (there are repeats in each of the 9 subarea). Need revision.</b-alert>
 		</p>
-		<div>
-			<table class="m-2 table table-bordered ">
-				<tr v-for="(n,i) in 9">
-					<td v-for="(m,j) in 9">
-						<b-form-input v-model="board[j + 9*i]" :disabled="cellDisabled(i,j)" :state="checkCellState(i,j)"></b-form-input>
+		<div class="board m-3">
+			
+			<table>
+				<tr  v-for="(n,i) in 9">
+					<td  v-for="(m,j) in 9" style="text-align:center">
+						<span v-if="cellDisabled(i,j)">{{board[j + 9*i]}}</span>
+						<b-btn @click="pickCellValue(i,j)" variant="secondary" class="boardbtn" v-if="!cellDisabled(i,j)">{{board[j + 9*i]}}</b-btn>
 					</td>
 				</tr>
 			</table>
@@ -109,6 +130,29 @@ const InGame = {
 			<b-btn variant="primary" @click="getNewGame()">New Game</b-btn>
 			<router-link to="/newgame"><b-btn variant="danger">Back</b-btn></router-link>
 		</div>
+		
+		<b-modal id="pick-cell-modal" hide-footer hide-header>
+			<b-button-group vertical >
+			    <b-button-group>
+			        <b-btn squared class="boardnumpadkey" variant="outline-dark" @click="numPadPressed('1')">1</b-btn>
+			        <b-btn squared class="boardnumpadkey" variant="outline-dark" @click="numPadPressed('2')">2</b-btn>
+			        <b-btn squared class="boardnumpadkey" variant="outline-dark" @click="numPadPressed('3')">3</b-btn>
+			    </b-button-group>
+			    <b-button-group>
+			        <b-btn squared class="boardnumpadkey" variant="outline-dark" @click="numPadPressed('4')">4</b-btn>
+			        <b-btn squared class="boardnumpadkey" variant="outline-dark" @click="numPadPressed('5')">5</b-btn>
+			        <b-btn squared class="boardnumpadkey" variant="outline-dark" @click="numPadPressed('6')">6</b-btn>
+			    </b-button-group>
+			    <b-button-group>
+			        <b-btn squared class="boardnumpadkey" variant="outline-dark" @click="numPadPressed('7')">7</b-btn>
+			        <b-btn squared class="boardnumpadkey" variant="outline-dark" @click="numPadPressed('8')">8</b-btn>
+			        <b-btn squared class="boardnumpadkey" variant="outline-dark" @click="numPadPressed('9')">9</b-btn>
+			    </b-button-group>
+			    <b-button-group>
+			        <b-btn squared class="boardnumpadkey" variant="outline-danger" @click="numPadPressed('')">Clear</b-btn>
+			    </b-button-group>
+		    </b-button-group>
+		</b-modal>
 		
 	</div>
 	`
