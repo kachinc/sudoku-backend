c8089d6706cdf7027e3c5281d3866ce104c74e69
kachinc <kcchongab@connect.ust.hk>
2020-08-31 05:15:04 +0800

    add env

--- src/main/java/com/kachinc/sudokubackend/controller/WebController.java
+++ src/main/java/com/kachinc/sudokubackend/controller/WebController.java
@@ -1,5 +1,7 @@
 package com.kachinc.sudokubackend.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +12,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WebController {
 	
+	@Autowired
+	private Environment environment;
+	
 	@GetMapping("/")
     public String home(Model model) {
+		model.addAttribute("env", environment.getActiveProfiles().length > 0 ? environment.getActiveProfiles()[0] : "dev");
         return "home";
     }
 
