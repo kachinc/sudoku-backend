5ce2117cfbf9a9bf0784457ade629916f254393b
kachinc <kcchongab@connect.ust.hk>
2020-08-31 05:39:21 +0800

    add build version, js cdn use version

--- src/main/java/com/kachinc/sudokubackend/controller/WebController.java
+++ src/main/java/com/kachinc/sudokubackend/controller/WebController.java
@@ -1,6 +1,8 @@
 package com.kachinc.sudokubackend.controller;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,14 +13,19 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class WebController {
-	
+
 	@Autowired
 	private Environment environment;
-	
+
+	@Autowired
+	BuildProperties buildProperties;
+
 	@GetMapping("/")
-    public String home(Model model) {
-		model.addAttribute("env", environment.getActiveProfiles().length > 0 ? environment.getActiveProfiles()[0] : "dev");
-        return "home";
-    }
+	public String home(Model model) {
+		model.addAttribute("env",
+				environment.getActiveProfiles().length > 0 ? environment.getActiveProfiles()[0] : "dev");
+		model.addAttribute("buildtime", buildProperties.getTime() == null ? RandomStringUtils.randomAlphabetic(5) : buildProperties.getTime().toEpochMilli());
+		return "home";
+	}
 
 }
